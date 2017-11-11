8b79ba4a9be51e5407f6051491a4d0362996dea5
Amberpl <mateusz.amb@gmail.com>
2017-11-11 23:38:25 +0100

    Zad 2

--- 01-elementarz_1/zad2.c
+++ 01-elementarz_1/zad2.c
@@ -1,29 +1,10 @@
+/* Program ten wypisuje liczby z tablicy tabela[]
+   w odwrotnej kolejności: 12, 6, 4, 2, 1 */
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-// program liczy pierwiastki równania a*x^2+b*x+c=0
-
-int main(int argc, char const *argv[]) {
-  printf("argc: %d\n", argc);
-  printf("argv[0] %s\n", argv[0]);
-  printf("argv[1] %s\n", argv[1]);
-  printf("argv[2] %s\n", argv[2]);
-  printf("argv[3] %s\n", argv[3]);
-
-  double a = atof(argv[1]);
-  double b = atof(argv[2]);
-  double c = atof(argv[3]);
-
-  double delta = b * b - 4 * a * c;
-
-  double x1 = (-b - sqrt(delta))/(2*a);
-  double x2 = (-b + sqrt(delta))/(2*a);
-
-  printf("Delta %f\n", delta);
-
-  printf("%.1f*x^2 1+ %.1f*x + %.1f = 0\n", a , b, c);
-
-  printf("x1 = %f\n", x1);
-  printf("x2 = %f\n", x2);
+int main() {
+   int tabela[] = {1, 2, 4, 6, 12};
+   for (int i=0; i < 5; i++) {
+    printf("%i ", tabela[4-i]);
+    }
+   return 0;
 }
