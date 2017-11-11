6328c24ad1b771469b0de2b8c9b6585e520b4267
Amberpl <mateusz.amb@gmail.com>
2017-11-11 23:26:07 +0100

    zad 1

--- 01-elementarz_1/zad1.c
+++ 01-elementarz_1/zad1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int result = 3* (2+2.0/5);
-  printf("wynik %d\n", result);
+  int n1 = 5+3*8/2-3;
+  int n2 = 2%2+2*2-2/2;
+  int n3 = 2*3*(5+9/2);
+  printf("Wyniki:\nn1=%d\nn2=%d\nn3=%d\n", n1, n2, n3);
 }
