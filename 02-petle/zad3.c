912f4be417cb83c7adae8eabb9e0476bf08bb104
Amberpl <mateusz.amb@gmail.com>
2017-11-21 19:26:26 +0100

    Gotowe zad 3

--- 02-petle/zad3.c
+++ 02-petle/zad3.c
@@ -4,14 +4,21 @@ int i;
 int a=0, b=0;
 
 int main() {
-  printf("Podaj pierwsza liczbe: \n a= ");
-  scanf("%d", &a);
-  printf("Podaj druga liczbe: \n b= ");
-  scanf("%d", &b);
+  while (a >= b) {
+    printf("Podaj pierwsza liczbe: \n a= ");
+    scanf("%d", &a);
+    printf("Podaj druga liczbe: \n b= ");
+    scanf("%d", &b);
 
-  if (a<b) {
-    for (i = a; i <= b; i++) {
-      printf("%d\n", i);
+    if (a<b) {
+      for (i = a; i <= b; i++) {
+        printf("%d\n", i);
+      }
+    }
+    else if (a>b)
+      printf("Sprobuj jeszcze raz a > b. \n\n");
+    else {
+      printf("Sprobuj jeszcze raz a = b. \n\n");
     }
   }
 }
