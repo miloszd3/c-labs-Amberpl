97658fead8b9650f5b3fbcce63b08aaf1befa779
Amberpl <mateusz.amb@gmail.com>
2017-11-22 23:32:04 +0100

    trzy figury

--- 02-petle/zad8.c
+++ 02-petle/zad8.c
@@ -1,10 +1,25 @@
 #include "stdio.h"
 
-int i, j;
+int i, j, k;
 
 int main() {
   for (i = 1; i < 5; i++) {
     for (j = 0; j < i; j++) {
+      putchar(42);
+    }
+    for (k = i; k < 8; k++) {
+      putchar(35);
+    }
+    for (j = i; j < 5; j++) {
+      putchar(42);
+    }
+    for (k = 0; k < i+1; k++) {
+      putchar(35);
+    }
+    for (k = 0; k < i+1; k++) {
+      putchar(35);
+    }
+    for (k = i; k < 5; k++) {
       putchar(42);
     }
     printf("\n");
