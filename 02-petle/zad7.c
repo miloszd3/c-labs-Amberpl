e017e1075aaff109b7b14a5ba1c4324ef8064e58
Amberpl <mateusz.amb@gmail.com>
2017-11-22 22:38:49 +0100

    zad 7

---
+++ 02-petle/zad7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+float liczby[6];
+int dodatnie;
+int i=1, j;
+
+int main() {
+  printf("Podj szesc liczb rzeczywistych:\n");
+  while (i<=6) {
+    printf("liczba [%i]=", i);
+    scanf("%f", &liczby[i-1]);
+    i++;
+  }
+
+  printf("\nWprowadzone liczby to:\n");
+  for (j = 0; j < 6; j++) {
+    printf("%.3f ", liczby[j]);
+  }
+    for (j = 0; j < 6; j++) {
+    if (liczby[j]>0) {
+      dodatnie++;
+    }
+  }
+  printf("\n\nIlosc liczb dodatnich w zbiorze: %i\n", dodatnie);
+}
