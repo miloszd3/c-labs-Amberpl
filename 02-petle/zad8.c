6f4dbd670a472acff3d3d6464aa4e3d375614b47
Amberpl <mateusz.amb@gmail.com>
2017-11-22 23:34:14 +0100

    Gotowe zadanie 8

--- 02-petle/zad8.c
+++ 02-petle/zad8.c
@@ -8,18 +8,24 @@ int main() {
       putchar(42);
     }
     for (k = i; k < 8; k++) {
-      putchar(35);
+      putchar(32);
     }
     for (j = i; j < 5; j++) {
       putchar(42);
     }
     for (k = 0; k < i+1; k++) {
-      putchar(35);
+      putchar(32);
     }
     for (k = 0; k < i+1; k++) {
-      putchar(35);
+      putchar(32);
     }
     for (k = i; k < 5; k++) {
+      putchar(42);
+    }
+    for (k = i; k < 8; k++) {
+      putchar(32);
+    }
+    for (j = 0; j < i; j++) {
       putchar(42);
     }
     printf("\n");
