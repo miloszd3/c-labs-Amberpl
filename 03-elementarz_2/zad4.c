fe5898fb280be676a96d4050793856c63c986c0b
Amberpl <mateusz.amb@gmail.com>
2017-11-24 22:57:24 +0100

    gotowe

---
+++ 03-elementarz_2/zad4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+  int c;
+  int nb, nt, nl;
+
+   nb = 0;  /* liczba znaków odstępu */
+   nt = 0;  /* liczba znaków tabulacji */
+   nl = 0;  /* liczba znaków nowego wiersza */
+
+   while ((c=getchar()) != EOF) {
+     if (c == ' ') {
+       ++nb;
+     }
+     else if (c == '\t') {
+       ++nt;
+     }
+     else if (c == '\n') {
+       ++nl;
+     }
+   }
+
+   printf("liczba znaków odstępu = %d, tabulacji = %d, nowego wiersza = %d\n", nb, nt, nl);
+}
